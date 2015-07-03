556635cc82a681a8b835b6b2370e9845bd89f697
niannian199 <niannian199@sina.com>
2015-07-03 19:59:21 +0800

    'init'

---
+++ src/com/jyn/acitiviylifedemo/MainActivity.java
@@ -0,0 +1,101 @@
+package com.jyn.acitiviylifedemo;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "ActivityDemo";
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.e(TAG, "start onStart~~~");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.e(TAG, "start onRestart~~~");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.e(TAG, "start onResume~~~");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.e(TAG, "start onPause~~~");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.e(TAG, "start onStop~~~");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.e(TAG, "start onDestroy~~~");
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.e(TAG, "start onCreate~~~");
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
